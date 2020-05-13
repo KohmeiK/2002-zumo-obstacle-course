36700a43ce7cbe962f31e193e908c59dc7481202
lbuermeyer <lbuermeyer@gmail.com>
2020-05-13 13:47:18 -0400

    Added comments to turn

--- CDR/KinematicTurn.h
+++ CDR/KinematicTurn.h
@@ -4,26 +4,27 @@
 
 class KinematicTurn{
   private:
-    double targetAngle;
-    uint16_t lastTime;
-    double wheelDiameter = WheelDiameter
-    double wheelBase = WheelBase;
-    double turningSpeed = TurningSpeed
-    int leftPos = 0;
-    int rightPos = 0;
-    double distPerTick = DistPerTick;
-    int updateInterval = KinematicTurnUpdateInterval;
-    Zumo32U4Encoders encodersTurn;
-    MotorSpeeds output;
-    float currentAngle = 0;
+    double targetAngle; //angle to be turned relative to starting angle
+    uint16_t lastTime; //time of last run, used to calculate change in time from one position to the next
+    double wheelDiameter = WheelDiameter //vertical height of tread
+    double wheelBase = WheelBase; //center to center tread distance
+    double turningSpeed = TurningSpeed //speed at which the wheels should turn
+    int leftPos = 0; //last known left wheel position
+    int rightPos = 0; //last known right wheel position
+    double distPerTick = DistPerTick; //linear distance robot travels per encoder tick
+    int updateInterval = KinematicTurnUpdateInterval; //how often the position and wheel speeds should be updated
+    Zumo32U4Encoders encodersTurn; //encoder object for turning
+    MotorSpeeds output; //object to store left and right target speeds
+    float currentAngle = 0; //the current angle of the robot
   public:
     //KinematicTurn(){}
     void Init(){
     }
-
+    
+    //checks if the robot has finished turning
     bool isFinished(){
-      bool finished = abs(currentAngle) >= abs(targetAngle);
-      if (finished)
+      bool finished = abs(currentAngle) >= abs(targetAngle); //checks if the robot has exceeded the target position
+      if (finished) //if the turn is complete, reset the encoders and return true
       {
         encodersTurn.getCountsAndResetLeft();
         encodersTurn.getCountsAndResetRight();
@@ -31,8 +32,8 @@ class KinematicTurn{
       return finished;
     }
 
-    void startTurn(int angle){
-      //update targetAngle
+    //sets the target angle, resets the encoders, and sets the starting time to begin turning
+    void startTurn(int angle){ 
       targetAngle = angle;
       lastTime = millis();
       currentAngle = 0;
@@ -43,8 +44,9 @@ class KinematicTurn{
       //Serial.println("starting turn to " + String(targetAngle) + " from current angle " + String(currentAngle));
     }
 
+    //motor direction determined by positive or negative turning angle. Speed determined from params.h
     MotorSpeeds calcSpeeds(){  
-      calculateAngle();
+      calculateAngle(); //updates current angle
       if (targetAngle > 0) 
       {
         output.left = -turningSpeed;
@@ -59,19 +61,19 @@ class KinematicTurn{
     }
     double calculateAngle() {
       uint16_t currTime = millis();
-      if (currTime - lastTime > updateInterval) 
+      if (currTime - lastTime > updateInterval) //if it is time to update the position
       {
-        double timeChange = float(currTime - lastTime) / 1000.;
+        double timeChange = float(currTime - lastTime) / 1000.;//calculate the change in time
         lastTime = currTime;
-        int currPosRight = encodersTurn.getCountsRight();
+        int currPosRight = encodersTurn.getCountsRight(); //get the new positions
         int currPosLeft = encodersTurn.getCountsLeft();
-        double speedRight = double(currPosRight - rightPos) * distPerTick / timeChange; // in/sec
-        double speedLeft = double(currPosLeft - leftPos) * distPerTick / timeChange; // in/sec
-        double angularVelocity = (speedRight - speedLeft) / wheelBase * 180 / PI; // deg/sec
+        double speedRight = double(currPosRight - rightPos) * distPerTick / timeChange; //calculate the linear speed of the right wheel in units of in/sec
+        double speedLeft = double(currPosLeft - leftPos) * distPerTick / timeChange; //calculate the linear speed of the left wheel in units of in/sec
+        double angularVelocity = (speedRight - speedLeft) / wheelBase * 180 / PI; //calculate the angular speed of the robot in units of deg/sec
         //Serial.println("prev Angle" + String(currentAngle));
-        currentAngle += angularVelocity * timeChange; //degrees
-        leftPos = currPosLeft;
-        rightPos = currPosRight;
+        currentAngle += angularVelocity * timeChange; //Update the current angle by adding the angular velocity * change in time
+        leftPos = currPosLeft; //store new left motor encoder position
+        rightPos = currPosRight; //store new left motor encoder position
         //Serial.println("next Angle" + String(currentAngle));
       }
       return currentAngle;
